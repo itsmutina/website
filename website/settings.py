5ca15858c7bc34e6a70c96d2bb658e98ea744473
itsmutina <80684421+itsmutina@users.noreply.github.com>
2022-09-05 11:20:07 +0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -19,7 +19,9 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
